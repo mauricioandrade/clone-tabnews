52ee9f3070ea040a57309340e47a560c58f7f5c1
Mauricio Andrade <mauricio_andrade@outlook.com>
2025-07-31 22:32:48 +0000

    adiciona scripts `lint:check ` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
-function Home(){
-    return <h1>O Infinito existe em todos os lugares - Satoru Gojo. 😎</h1>
+function Home() {
+  return <h1>O Infinito existe em todos os lugares - Satoru Gojo. 😎</h1>;
+}
+
+function teste() {
+  console.log("teste");
 }
 
 export default Home;
