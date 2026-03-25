3b9bffe626704c005edded07917a6415eade9e41
mauricioandrade <mauricio_andrade@outlook.com>
2026-03-25 20:52:10 -0300

    adiciona scipts lint:check e lint:fix

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>Aqueles que seguem o caminho da vingança nunca acabam bem, você só se destrói. - Hatake Kakashi</h1>
+  return (
+    <h1>
+      Aqueles que seguem o caminho da vingança nunca acabam bem, você só se
+      destrói. - Hatake Kakashi
+    </h1>
+  );
 }
 
 export default Home;
